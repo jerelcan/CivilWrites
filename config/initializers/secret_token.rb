21eb7f4f11244794ce5c56aeb724987bd150b882
Jerel Canales <jerelcan@gmail.com>
2011-09-17 15:08:53 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CivilWrites::Application.config.secret_token = '8d36aacc483bb1491765bde10293a2eff05d18b94f0de6840693b87c8104879b66ead8c07221ddc9295f3df6963db63020245c6b66664902fd7e2b81932afe6c'
