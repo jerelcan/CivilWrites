a3aa3ea5f9019c87bdd27e4969819144a9cab1dc
Jerel Canales <jerelcan@gmail.com>
2011-09-25 17:10:30 -0700

    Finished first cut of the User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 CivilWrites::Application.routes.draw do
+
+  resources :users
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+  match '/signup',  :to => 'users#new'
   root              :to => 'pages#home'
 
 
+#  get "users/new"
   get "pages/home"
 
   get "pages/contact"
