0cd84cc24bbddde02a8aed3c8dcc25e07183e61c
Jerel Canales <jerel.canales@sv.cmu.edu>
2011-10-19 17:56:49 -0700

    user password intermediate release

--- Gemfile
+++ Gemfile
@@ -6,9 +6,12 @@ gem 'rails', '3.0.9'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3', '1.3.3'
+gem 'devise'
+gem 'gravatar_image_tag', '1.0.0.pre2'
 
 group :development do
   gem 'rspec-rails', '2.6.1'
+  gem 'annotate', '2.4.0'
 end
 
 group :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,24 @@
 CivilWrites::Application.routes.draw do
+  resources :users
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
   root              :to => 'pages#home'
-
 
   get "pages/home"
 
   get "pages/contact"
 
   get "pages/about"
+
+  get "pages/help"
+
+  resources :users
+
+  match '/signup',  :to => 'users#show'
+
+  get "users/signup"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
